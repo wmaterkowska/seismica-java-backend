e98756e0dec75140b5fbe52f9d37835f14a57532
Weronika Materkowska <w.m.materkowska@gmail.com>
2023-08-17 19:21:48 +0200

    initial commit

---
+++ src/main/java/com/springbootapp/seismica/SeismicaApplication.java
@@ -0,0 +1,13 @@
+package com.springbootapp.seismica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeismicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeismicaApplication.class, args);
+	}
+
+}
